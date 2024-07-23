5dea5cf419d06e888c0f3792d4a01d05daa3c95c
Santiago Varela <santiago.vrl.12@gmail.com>
2024-07-23 16:58:40 -0300

    correccion correo electronico

--- BackEnd/backend-portfolio/src/main/java/com/backend/portfolio/backend_portfolio/services/emailService.java
+++ BackEnd/backend-portfolio/src/main/java/com/backend/portfolio/backend_portfolio/services/emailService.java
@@ -1,22 +1,30 @@
 package com.backend.portfolio.backend_portfolio.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Component
 public class emailService {
 
-@Autowired
+    @Autowired
     private JavaMailSender emailSender;
 
     public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage(); 
-        message.setFrom("santiago.varela@hotmail.com");
-        message.setTo(to); 
-        message.setSubject(subject); 
-        message.setText(text);
-        emailSender.send(message);
+        MimeMessage message = emailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+            helper.setFrom("santiago.varela@hotmail.com");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
