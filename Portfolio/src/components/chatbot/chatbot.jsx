6b60e14098332e1a0173d87db6ae2e4d3942b530
Santiago Varela <santiago.vrl.12@gmail.com>
2024-12-03 16:00:37 -0300

    conect back

--- Portfolio/src/components/chatbot/chatbot.jsx
+++ Portfolio/src/components/chatbot/chatbot.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import "../chatbot/chatbot.css"
 
-const apiBaseURL = import.meta.url
+const apiBaseURL = "https://backend-portfolio-latest.onrender.com";
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
